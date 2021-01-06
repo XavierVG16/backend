bac50348421d2fcb4dda80523c318f6d295319ee
xavier VG <kjaviervelasque16@gmail.com>
2021-01-05 20:48:48 -0500

    make it better

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,52 @@
+const pool = require("../database");
+const jwt = require("jsonwebtoken");
+const helpers = require("../lib/helper");
+const authCtrl = {};
+authCtrl.signin = async (req, res, next) => {
+    const { email, pass_usuario } = req.body
+console.log(req.body)
+    try {
+        const usuarioC = await pool.query('select * from usuarios where email = ?', email)
+console.log(usuarioC)
+        if (usuarioC.length == 0) {
+            return res.status(400).json({ message: 'Usuario no encontrado' })
+
+
+        } else {
+            const user = usuarioC[0];
+            const validPassword = await helpers.matchPassword(pass_usuario, user.pass_usuario);
+            if (!validPassword) {
+                return res.status(401).json({
+
+                    message: "Contraseña incorrecto",
+                });
+
+
+            }
+            const token = jwt.sign({ id: user.id_usuario }, process.env.SECRET);
+
+            res.status(200).send({
+                token: token
+            });
+
+        }
+    } catch (error) {
+        console.log(error);
+    }
+
+
+}
+
+authCtrl.User = async (req, res, next) => {
+
+    const { id } = req.params;
+    const token = id
+
+    const decoded = jwt.verify(token, process.env.SECRET);
+    console.log(decoded.id)
+    const user = await pool.query('select * from usuarios where id_usuario = ?', decoded.id)
+
+    res.json(user[0])
+
+}
+module.exports = authCtrl;
---
+++ src/index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const cors = require('cors');
+const MysQlStore = require("express-mysql-session");
+const session = require("express-session");
+const path = require("path");
+const multer = require("multer");
+
+const app = express();
+require("dotenv").config();
+
+require('./database');
+const { database } = require("./keys");
+// Settings
+
+app.set('port', process.env.PORT || 3000);
+
+// Middlewares
+
+app.use(cors({ origin: process.env.CROSS_HOST }));
+
+app.use(express.json());
+app.use(
+  session({
+    secret: "xavier",
+    resave: false,
+    saveUninitialized: false,
+    store: new MysQlStore(database),
+    cookie: { secure: true }
+  })
+);
+
+const storage = multer.diskStorage({
+  destination: path.join(__dirname, "public/img/uploads"),
+  filename: (req, file, cb) => {
+    cb(null, new Date().getTime() + path.extname(file.originalname));
+  },
+});
+app.use(multer({ storage }).single("image"));
+
+
+
+// Routes
+app.use('/',require('./routes/auth.route'));
+app.use('/usuario',require('./routes/usuario.route'));
+app.use('/categoria',require('./routes/categorias.route'));
+app.use('/lector',require('./routes/lector.route'));
+app.use('/libro',require('./routes/libro.route'));
+app.use('/prestamo',require('./routes/prestamo.route'));
+
+
+// starting the server
+app.listen(app.get('port'), () => {
+  console.log(`server on port ${app.get('port')}`);
+  console.log("environment:", process.env.NODE_ENV);
+});
