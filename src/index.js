11d4c761a72a88bd8013a274137f80589a74d565
xavier VG <kjaviervelasque16@gmail.com>
2021-01-07 21:04:29 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const { database } = require("./keys");
 app.set('port', process.env.PORT || 3000);
 // Configurar cabeceras y cors
 app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', process.env.CROSS_HOST);
+	res.header('Access-Control-Allow-Origin', `${process.env.CROSS_HOST}`);
 	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
 	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
