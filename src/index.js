62dcf59294402497a71b6deed434f28580a490e2
xavier VG <kjaviervelasque16@gmail.com>
2021-01-08 20:08:01 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -15,14 +15,18 @@ const { database } = require("./keys");
 app.set('port', process.env.PORT || 3000);
 // Configurar cabeceras y cors
 app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', `${process.env.CROSS_HOST}`);
-	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  return res.status(200).json({});
+  res.header('Access-Control-Allow-Origin', '*');
 
-	next();
+  // authorized headers for preflight requests
+  // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+
+  app.options('*', (req, res) => {
+      // allowed XHR methods  
+      res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+      res.send();
+  });
 });
 // Middlewares
 
