2c3c10f597159848c0b4eb930844a3cef75f3e20
xavier VG <kjaviervelasque16@gmail.com>
2021-01-08 20:46:08 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -15,14 +15,14 @@ const { database } = require("./keys");
 app.set('port', process.env.PORT || 3000);
 // Configurar cabeceras y cors
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', `${process.env.CROSS_HOST}`);
 
   // authorized headers for preflight requests
   // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 
-  app.options('*', (req, res) => {
+  app.options(`${process.env.CROSS_HOST}`, (req, res) => {
       // allowed XHR methods  
       res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
       res.send();
