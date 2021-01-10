de569a94981d3bf233de765ae7da2f14757c21d8
xavier VG <kjaviervelasque16@gmail.com>
2021-01-10 15:58:03 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -71,32 +71,25 @@ var corsOptions = {
         return callback(null, true);
     }
 }
+
+
  */
 // Middlewares
 
-const whitelist = ['https://sistemabiblioteca-vl.herokuapp.com',
-'http://localhost:4200',
-`${process.env.PORT}`]; // list of allow domain
-
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin) {
-            return callback(null, true);
-        }
-
-        if (whitelist.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}
-
-// end 
-app.use(cors(corsOptions));
-
+var host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || 8080;
  
+var cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
+
+
 app.use(express.json());
 app.use(
   session({
@@ -128,7 +121,9 @@ app.use('/api/prestamo',require('./routes/prestamo.route'));
 
 
 // starting the server
-app.listen(app.get('port'), () => {
+/**
+ * app.listen(app.get('port'), () => {
   console.log(`server on port ${app.get('port')}`);
   console.log("environment:", process.env.NODE_ENV);
 });
+ */
