b7eab4bbf7cb8dfb43e6041edd073a2d170a1c8a
xavier VG <kjaviervelasque16@gmail.com>
2021-01-09 19:15:36 -0500

    first commit

--- src/cors.js
+++ src/cors.js
@@ -1,6 +1,15 @@
 function AllowCrossDomain(){
     this.permisos = function (req, res, nex){
-        res.header('Access-Control-Allow-Origin', `${process.env.CROSS_HOST}`);
+        var whileList = [
+            'https://sistemabiblioteca-vl.herokuapp.com',
+            'http://localhost:4200'
+
+        ];
+        var origen = req.header.origen;
+        if(whileList.indexOf(origen)>= -1){
+            res.header('Access-Control-Allow-Origin', origen);
+
+        }
         res.header('Access-Control-Allow-Header', 'Content-Type');
         res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
         next();
