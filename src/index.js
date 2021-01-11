14eac67b9d378fec95cee85177a33c09dcfc6d57
xavier VG <kjaviervelasque16@gmail.com>
2021-01-11 16:49:49 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -56,6 +56,8 @@ app.use(function (req, res, next){
   next();
 
 })
+ */
+// Middlewares
 
 var whitelist = ['http://localhost:4200', 'http://localhost:4202', 'https://sistemabiblioteca-vl.herokuapp.com']
 var corsOptions = {
@@ -69,31 +71,9 @@ var corsOptions = {
   }
 }
  
-app. use(cors({origin: process.env.CROSS_HOST }));
- */
-// Middlewares
-
-app.use(function (req, res, next) {
-  /*var err = new Error('Not Found');
-   err.status = 404;
-   next(err);*/
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', process.env.CROSS_HOST ||'*');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
-
-//  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-  // Pass to next layer of middleware
-  next();
-});
-
-
+app. use(cors({origin: process.env.CROSS_HOST,
+  reflightContinue: true
+ }));
 
 
  
